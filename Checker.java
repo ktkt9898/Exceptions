e877e77b95592fcea13ed38172be1edc6d41938a
ktkt9898 <kyletruschel@u.boisestate.edu>
2024-09-05 15:00:47 -0600

    Exceptions Project started

---
+++ Checker.java
@@ -0,0 +1,89 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.lang.NumberFormatException;
+
+
+/**
+ * This class performs the valid checking of files
+ * 
+ * @author Kyle Truschel
+ */
+public class Checker {
+    private String stringFileName;
+
+    public Checker( String fileName) throws FileNotFoundException {
+        readFile( fileName );
+    }
+
+    public boolean readFile( String stringFileName ) throws FileNotFoundException {
+        String error = "";
+        this.stringFileName = stringFileName;
+
+        File fileName = new File(stringFileName);
+
+        Scanner fileScanner = new Scanner(fileName);
+
+        String line = fileScanner.nextLine();
+
+        // Try to create and open a file
+        try {
+            Scanner lineScanner = new Scanner(line);
+
+            if (lineScanner.hasNextInt()) {
+                int startRows = lineScanner.nextInt();
+                if (lineScanner.hasNextInt()) {
+                    int startCols = lineScanner.nextInt();
+                    lineScanner.nextLine();
+
+                    for (int row = 1; row < startRows; row++) {
+                        String skip = lineScanner.nextLine();
+                        String[] skipArray = skip.split(" ");
+
+                        for (int col = 1; col < startCols; col++) {
+                            if (lineScanner.hasNextDouble( )){
+                                lineScanner.nextDouble( );
+                            }
+    
+                            else {
+                                error = lineScanner.next();
+                                throw new NumberFormatException();
+                            }
+                        }
+                        if (lineScanner.hasNextDouble( )) {
+                            lineScanner.nextDouble( );
+                        }
+    
+                        else {
+                            error = lineScanner.next();
+                            throw new NumberFormatException();
+                        }
+                    }
+                }
+            }
+            else {
+                return false;
+            }
+
+            lineScanner.close();
+            fileScanner.close();
+            return true;
+
+        } catch (NoSuchElementException snee) {
+            System.out.println("No snee Bad");
+            fileScanner.close();
+            return false;
+
+        } catch (NumberFormatException nfe) {
+            System.out.println("java.lang.NumberFormatException: For input string: " + "\"" + error + "\"");
+            fileScanner.close();
+            return false;
+
+        } catch (Exception e) {
+            System.out.println("No e Bad");
+            fileScanner.close();
+            return false;
+        }
+    }
+}
--- FormatChecker.java
+++ FormatChecker.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 
 /**
  * This acts as the driver
@@ -11,7 +12,9 @@ public class FormatChecker {
     public static void printUsage ( ) {
         System.out.println("Usage: java FormatChecker <file_name>");
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         
+        Checker test = new Checker("valid1.dat");
+        test.readFile("valid1.dat");
     }
 }
